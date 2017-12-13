dc5108ceb26fb2d64bd10c908d68e37d5fff5339
shreyashankar19 <shreya.shankar@duke.edu>
2017-12-13 16:49:53 -0500

    Update my_code.ino

--- code/my_code.ino
+++ code/my_code.ino
@@ -1,23 +1,37 @@
 // My Project Code
-// Your Name
-// Date
+// Shreya Shankar
+// December 13, 2017
 
-// Cite your sources, e.g. "Code adapted from http://____"
+// Cite your sources, e.g. "Code adapted from https://learn.adafruit.com/force-sensitive-resistor-fsr/using-an-fsr"
 
-// Libraries
+int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
+int fsrReading;     // the analog reading from the FSR resistor divider
+ 
+void setup(void) {
 
-#include "some_library.h"
-
-// variables
-int variable;
-
-
-// setup
-void setup(){
+  Serial.begin(9600);   
+  Particle.variable("Force", fsrReading);
+  // Spark.variable("getForce", &fsrPin, INT);
 
 }
-
-// loop
-void loop(){
-
-}
+ 
+void loop(void) {
+  fsrReading = analogRead(fsrPin);  
+ 
+  Serial.print("Analog reading = ");
+  Serial.print(fsrReading);     // the raw analog reading
+  
+fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
+  Serial.print("Voltage reading in mV = ");
+  Serial.println(fsrVoltage);  
+ 
+    // The voltage = Vcc * R / (R + FSR) where R = 10K and Vcc = 5V
+    // so FSR = ((Vcc - V) * R) / V       
+    fsrResistance = 5000 - fsrVoltage;     // fsrVoltage is in millivolts so 5V = 5000mV
+    fsrResistance *= 10000;                // 10K resistor
+    fsrResistance /= fsrVoltage;
+    Serial.print("FSR resistance in ohms = ");
+    Serial.println(fsrResistance)
+ 
+  delay(1000);
+} 
